4d11e1eb2ecb6feba276b05fbd7926a142c8f01f
AntohaGaranin <garanin.a@yahoo.com>
2022-08-30 22:05:30 +0300

    add solution for task 0,1,3,5,7

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿//========================================================================
+// # 3 Напишите программу, которая будет
+// является ли первое число квадратом второго
+//========================================================================
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+
+    int inputDayofWeek = int.Parse(inputLine);
+
+    string [] dayOfWeek = new string[7];
+    dayOfWeek[0] = "Понедельник";
+    dayOfWeek[1] = "Вторник";
+    dayOfWeek[2] = "Среда";
+    dayOfWeek[3] = "Четверг";
+    dayOfWeek[4] = "Пятница";
+    dayOfWeek[5] = "Суббота";
+    dayOfWeek[6] = "Воскресенье";
+
+if(inputDayofWeek > 7 || inputDayofWeek < 1)
+{
+    Console.WriteLine("Такого дня нет");
+}
+    Console.WriteLine(dayOfWeek[inputDayofWeek-1]);
+}
