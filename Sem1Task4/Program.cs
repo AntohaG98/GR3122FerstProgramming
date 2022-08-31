7b4c37a29c00cc450651a319cbf6804dc6caaf2a
AntohaGaranin <garanin.a@yahoo.com>
2022-08-31 19:14:07 +0300

    AddedHomeWork Tasks 2-4-6-8

---
+++ Sem1Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿//===========================================================================
+// # 4 Напишите программу, которая на вход принимает три числа и 
+// выдает максимальное из этих чисел
+//===========================================================================
+
+//Вводим три числа
+Console.Write("Введите число A:");
+string? inputLineA = Console.ReadLine();
+Console.Write("Введите число B:");
+string? inputLineB = Console.ReadLine();
+Console.Write("Введите число C:");
+string? inputLineC = Console.ReadLine();
+
+if (inputLineA != null && inputLineB != null && inputLineC != null) //Проверяем, что на входе не пустые данные, и приступаем к сравнению
+{
+    int A = int.Parse(inputLineA);
+    int B = int.Parse(inputLineB);
+    int C = int.Parse(inputLineC);
+    if (A > B)
+    {
+        if(A > C)
+        {
+            Console.Write("A - максимальное число");
+        }
+        else
+        {
+            Console.Write("C - максимальное число");
+        }
+    }
+    else
+    {
+        if(B > C)
+        {
+            Console.Write("B - максимальное число");
+        }
+        else
+        {
+            Console.Write("C - максимальное число");
+        }
+    }
+}
