0b1df5aced28a680db2a0047d907616d59a7c21b
AntohaGaranin <garanin.a@yahoo.com>
2022-09-02 21:51:26 +0300

    Seminar2

---
+++ Sem2Task11/Program.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------------
+//                                  Задача 11
+// Напишите программу, которая выводит случайное трехзначное число 
+// и удаляет вторую цифру этого числа.
+//-----------------------------------------------------------------------------------
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine(number);
+int firstDigit = number/100;
+int thirdDigit = number%10;
+int outputNumber = firstDigit*10 + thirdDigit;
+Console.WriteLine(outputNumber);
+
+//Вариант Евгения
+System.Random numberGenerator = new System.Random();
+int number = numberGenerator.Next(100, 1000);
+Console.WriteLine(number);
+int firstDigit = number / 100;
+int thirdDigit = number % 10;
+Console.WriteLine(firstDigit*10+thirdDigit);
+
+//Вариант Михаила
+void MyVariant()
+{
+    Console.WriteLine("Метод 1");
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+    Console.WriteLine(number);
+    char [] charArray = number.ToString().ToCharArray();
+    Console.WriteLine("" + charArray[0] + charArray[2]);
+}
+MyVariant();
+
+//Вариант Константина
+void Variant1()
+{
+    Console.WriteLine("Вариант 1:");
+    int number = new Random().Next(100, 1000);
+    Console.WriteLine(number);
+    char[] charArray = number.ToString().ToCharArray();
+    Console.Write(charArray[0]);
+    Console.WriteLine(charArray[2]);
+}
+Variant1();
---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,92 @@
+﻿//----------------------------------------------------------------------------------------------------------------------
+//                                  Задача 12
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому.
+// Если второе число некратно первому, то программа выводит остаток от деления.
+//----------------------------------------------------------------------------------------------------------------------
+
+// Console.Write("Введите число A:");
+// string? inputLineA = Console.ReadLine();
+// Console.Write("Введите число B:");
+// string? inputLineB = Console.ReadLine();
+
+// int A = int.Parse(inputLineA);
+// int B = int.Parse(inputLineB);
+
+// bool R = (B % A == 0);
+
+// if (R)
+// {
+//     Console.WriteLine("Число B кратно A");
+// }
+// else
+// {
+//     Console.WriteLine("Не кратно, " + B % A);
+// }
+
+int inputNumberA = 0;
+int inputNumberB = 0;
+bool result = false;
+
+ReadData();
+ConculateData();
+PrintData();
+
+//Получаем два числа от пользователя
+void ReadData()
+{
+    Console.Write("Введите первое число: ");
+    string? inputLineA = Console.ReadLine();
+    Console.Write("Введите второе число: ");
+    string? inputLineB = Console.ReadLine();
+
+    int inputNumberA = int.Parse(inputLineA);
+    int inputNumberB = int.Parse(inputLineB);
+}
+
+//Определяем кратность чисел
+void ConculateData()
+{
+    result = (inputNumberB % inputNumberA == 0);
+}
+
+//Выводим данные вычисления
+void PrintData()
+{
+    if (result)
+    {
+        Console.WriteLine("Второе число кратно первому");
+    }
+    else
+    {
+        Console.WriteLine("Остаток от деления: " + inputNumberB % inputNumberA);
+    }
+}
+
+
+
+//Вариант Евгения
+// string? inputLineA = Console.ReadLine();
+// string? inputLineB = Console.ReadLine();
+
+// if (inputLineA != null && inputLineB != null)
+// {
+//     int inputNumberA = (int)int.Parse(inputLineA);
+//     int inputNumberB = (int)int.Parse(inputLineB);
+
+//     Console.WriteLine(inputNumberB%inputNumberA==0?"Является кратным":inputNumberB%inputNumberA);
+// }
+
+//вариант Константина
+// void Variant1()
+// {
+//     Console.Write("Введите первое число: ");
+//     string? inputLineA = Console.ReadLine();
+//     Console.Write("Введите второе число: ");
+//     string? inputLineB = Console.ReadLine();
+
+//     int inputNumberA = int.Parse(inputLineA);
+//     int inputNumberB = int.Parse(inputLineB);
+
+//     Console.WriteLine((inputNumberB % inputNumberA == 0) ? ("Второе число кратно первому") : ("Остаток от деления: " + inputNumberB % inputNumberA));
+// }
+// Variant1();
