fa581b13689bd743786d8fb1e24963d65b929e9f
AntohaGaranin <garanin.a@yahoo.com>
2022-09-09 19:42:41 +0300

    Homework №3 Tasks 19-21-23

---
+++ Sem3Task23/Program.cs
@@ -0,0 +1,34 @@
+﻿// -------------------------------------------------------------------------------
+//                           Задача 23
+// Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу кубов чисел от 1 до N.
+// -------------------------------------------------------------------------------
+
+// Ввод данных
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Создание первой строчки от 1 до N и второй строчки от 1 до N^3 с вывводом на экран
+void LineNumberPow(int numberN, int pow)
+{
+    int i = 1;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        Console.Write(Math.Pow(i, pow) + "\t");
+        i++;
+    }
+    Console.WriteLine(Math.Pow(i, pow) + "\t");
+}
+
+int num = ReadData("Введите число N:");
+
+LineNumberPow(num, 1);
+LineNumberPow(num, 3);
