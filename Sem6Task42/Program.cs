dd596ac2033d91979833e303cbd234397d194f35
AntohaGaranin <garanin.a@yahoo.com>
2022-09-18 16:56:12 +0300

    Seminar 6 Tasks 39-40-42-44-45

---
+++ Sem6Task42/Program.cs
@@ -0,0 +1,44 @@
+﻿//-----------------------------------------------
+//                Задача 42
+// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+//-----------------------------------------------
+
+//Вариант Константина
+// Метод считывания данных пользователя
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Метод, принимает строку, выводит в консоль
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+string DecToBin(int number)
+{
+    string binNumber = "";
+
+    while (number > 0)
+    {
+        binNumber = number % 2 + binNumber;
+        number = number / 2;
+    }
+
+    return binNumber;
+}
+
+int inputNumber = ReadData("Введите число: ");
+
+PrintResult($"Число {inputNumber} в двоичном представлении: {DecToBin(inputNumber)}");
+
+
+//int numb = ReadData("Введите число: ");
+//string numbBin = Convert.ToString(numb, 2);
---
+++ Sem6Task44/Program.cs
@@ -0,0 +1,51 @@
+﻿//----------------------------------------------------------
+//                      Задача 44
+// Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: 0 и 1.
+//----------------------------------------------------------
+
+// Вариант Анатолия
+// Создание словаря.
+Dictionary<int, double> fiboDict = new Dictionary<int, double>
+{
+    { 0, 1 },
+    { 1, 1 }
+};
+
+// Фибоначи рекурсия.
+double FibonachiRec(int numb, Dictionary<int, double> fiboDict)
+{
+    if (fiboDict.ContainsKey(numb))
+        return fiboDict[numb];
+
+    else
+    {
+        double fiboSum = FibonachiRec(numb - 2, fiboDict) + FibonachiRec(numb - 1, fiboDict);
+        fiboDict.Add(numb, fiboSum);
+        return fiboSum;
+    }
+}
+
+// Фибоначи цикл
+double FibonachiСycle(int numb)
+{
+    double fiboPrev = 1;
+    double fibo = 1;
+    for (int i = 2; i <= numb; i++)
+    {
+        double tmp = fibo;
+        fibo += fiboPrev;
+        fiboPrev = tmp;
+    }
+    return fibo;
+}
+
+
+int fiboNumb = 50;
+
+
+for (int i = 0; i <= fiboNumb; i++)
+{
+    Console.WriteLine($"{i} --> {FibonachiRec(i, fiboDict)}");
+    Console.WriteLine($"{i} --> {FibonachiСycle(i)}");
+}
